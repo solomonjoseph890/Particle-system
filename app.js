69004fde249e0d1570a3a30aa5b3e38713128cdc
Joseph <solomonjoseph890@gmail.com>
2025-11-01 02:19:32 +0000

    1st Particle System Done

---
+++ app.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("hello");
+  const box = [];
+  console.log(box.length);
+
+  console.log(box);
+  const boxGrid = document.querySelector(".box-grid");
+
+  console.log(boxGrid.children);
+
+  for (let index = 500; index > box.length; index--) {
+    const plus = document.createElement("p");
+    plus.classList.add("particle");
+    plus.innerText = "+";
+    box.push(plus);
+    boxGrid.append(plus);
+
+    plus.addEventListener("mouseover", () => {
+      plus.style.color = "black";
+      setTimeout(() => {
+        plus.style.color = "blue";
+      }, 500);
+    });
+
+    console.log(box.length);
+    console.log(boxGrid.children);
+  }
+});
